d7e3a6bce5781cb28d687cc6b0f5fbcfcb4f4210
yuyadatki <yuya.tanakaa557@gmail.com>
2025-09-16 10:45:49 -0700

    test

--- twikit/constants.py
+++ twikit/constants.py
@@ -28,17 +28,20 @@ FEATURES = {
 }
 
 USER_FEATURES = {
-    'hidden_profile_likes_enabled': True,
-    'hidden_profile_subscriptions_enabled': True,
-    'responsive_web_graphql_exclude_directive_enabled': True,
-    'verified_phone_label_enabled': False,
-    'subscriptions_verification_info_is_identity_verified_enabled': True,
-    'subscriptions_verification_info_verified_since_enabled': True,
-    'highlights_tweets_tab_ui_enabled': True,
-    'responsive_web_twitter_article_notes_tab_enabled': False,
-    'creator_subscriptions_tweet_preview_api_enabled': True,
-    'responsive_web_graphql_skip_user_profile_image_extensions_enabled': False,
-    'responsive_web_graphql_timeline_navigation_enabled': True
+  "hidden_profile_subscriptions_enabled": True,
+  "payments_enabled": False,
+  "profile_label_improvements_pcf_label_in_post_enabled": True,
+  "rweb_tipjar_consumption_enabled": True,
+  "verified_phone_label_enabled": False,
+  "subscriptions_verification_info_is_identity_verified_enabled": True,
+  "subscriptions_verification_info_verified_since_enabled": True,
+  "highlights_tweets_tab_ui_enabled": True,
+  "responsive_web_twitter_article_notes_tab_enabled": True,
+  "subscriptions_feature_can_gift_premium": True,
+  "creator_subscriptions_tweet_preview_api_enabled": True,
+  "responsive_web_graphql_skip_user_profile_image_extensions_enabled": False,
+  "responsive_web_graphql_timeline_navigation_enabled": True
+
 }
 
 LIST_FEATURES = {
